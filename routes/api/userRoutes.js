c136ad3add2649ac6ec4c1f0e93cb88ff27299c6
aheath <aaron.heath5447@gmail.com>
2024-02-04 18:26:08 -0500

    Implement get and post routes to get all users and create one user respectively

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const { User, Thought } = require('../../models')
 
-router.get('/', (req,res) => {
-    res.send('Hello, world!');
+router.get('/', async (req,res) => {
+    // Get all users
+    try {
+        const users = await User.find({});
+        res.status(200).json(users);    
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error:"Something went wrong"});
+
+    }
+    
+});
+
+router.post('/', async (req, res) => {
+    // Create a new user
+    const newUser = new User({
+       username: req.body.username,
+       email: req.body.email,
+    });
+
+    newUser.save();
+
+    if (newUser){
+        res.status(201).json(newUser);
+    } else {
+        console.log("Something went wrong.");
+        res.status(500).json({error: "Something went wrong."});
+    }
 });
 
 module.exports = router;
