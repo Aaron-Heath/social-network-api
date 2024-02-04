5f3d99a8130838a39c6a4bc5fb579a54090cc785
aheath <aaron.heath5447@gmail.com>
2024-02-04 17:38:18 -0500

    export schema. Remove comments

--- models/Reaction.js
+++ models/Reaction.js
@@ -18,20 +18,6 @@ const reactionSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-})
+});
 
-// Use Mongoose's ObjectId data type
-    // Default value is set to a new ObjectId
-
-// reactionBody
-    // String
-    // Required
-    // 280 character maximum
-
-// username
-    // String
-    // Required
-// createdAt
-    // Date
-    // Set default value to the current timestamp
-    // Use a getter method to format the timestamp on query
+module.exports = reactionSchema;
