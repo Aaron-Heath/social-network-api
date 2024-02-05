ca9b542a8d6588828b9a501cbb0860a0e46fdad5
aheath <aaron.heath5447@gmail.com>
2024-02-04 21:49:18 -0500

    Finish thoughts routes. Implement get single thought by Id

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -17,6 +17,21 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get("/:thoughtId", async (req,res)=> {
+    try{
+        const thought = await Thought.findOne({_id: req.params.thoughtId});
+
+        if(!thought) {
+            return res.status(404).json({message: "No record found"});
+        }
+
+        res.json(thought);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({message: "Something went wrong."});
+    }
+});
+
 router.post('/', async (req, res) => {
 
 
