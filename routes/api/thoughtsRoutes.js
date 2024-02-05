3bf4f3a7b528da33739e346e73c8910443ab638d
aheath <aaron.heath5447@gmail.com>
2024-02-04 21:09:43 -0500

    Add thoughtsRoutes to index.js. Implement get all thoughts and create new thought

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,46 @@
+const router = require('express').Router()
+const { User, Thought } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find({});
+
+        if(!thoughts) {
+            return res.status(404).json({message: "no record found"});
+        }
+
+        return res.json(thoughts);
+
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({message: "Something went wrong."})
+    }
+});
+
+router.post('/', async (req, res) => {
+
+
+    // {
+    //     "thoughtText": "Here's a cool thought...",
+    //     "username": "lernantino",
+    //     "userId": "5edff358a0fcb779aa7b118b"
+    //   }
+    try {
+        // Create a new thought
+        const newThought = await Thought.create({
+            thoughtText: req.body.thoughtText,
+            username: req.body.username
+        });
+
+        // Push thought to user
+        const user = await User.findOneAndUpdate({_id: req.body.userId},
+            {$push: {thoughts: newThought._id}});
+        
+        return res.json(newThought);
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({message:"Something went wrong."});
+    }
+});
+
+module.exports = router;
